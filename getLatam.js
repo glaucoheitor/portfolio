c34ac02f9e2888157f706431c4a783c0ba739a28
Glauco Marinho <glaucoheitor@gmail.com>
2022-01-11 14:34:27 -0800

    latam test

--- getLatam.js
+++ getLatam.js
@@ -13,7 +13,8 @@ const res = await fetch(
   "https://bff.latam.com/ws/api/bookingbox-services/v1/destinations?airline=tam&portal=pessoas&application=fidelidade&applicationName=shopping&country=BR&language=pt&step=2&origin=city::PAR"
 );
 
-let cookie = res.headers.get("set-cookie");
+let cookies = res.headers.raw()["set-cookie"];
+let cookie = cookies.map((cookie) => cookie.split(";")[0]).join("; ");
 
 const { data } = await res.json();
 
@@ -21,28 +22,34 @@ const airportCodes = data.places.map((place) => place.iataCode);
 
 //console.log(airportCodes);
 let flights = [];
-
+cookie = "";
 for (const airportCode of airportCodes) {
   const controller = new AbortController();
   console.log(airportCode);
   console.log(cookie);
-  const flightsDataRes = await fetchTimeout(
-    `https://bff.latam.com/ws/proxy/booking-webapp-bff/v1/public/redemption/recommendations/oneway?departure=2022-02-03&origin=PAR&destination=${airportCode}&cabin=Y&country=BR&language=PT&home=pt_br&adult=1&tierCode=LTAM&tierType=low`,
-    4000,
+  const flightsDataRes = await fetch(
+    `https://www.latamairlines.com/bff/air-offers/offers/search?sort=RECOMMENDED&cabinType=Economy&origin=PAR&destination=${airportCode}&inFlightDate=null&inFrom=null&inOfferId=null&outFlightDate=null&outFrom=2022-02-23&outOfferId=null&adult=1&child=0&infant=0&redemption=false`,
+    //4000,
     {
       credentials: "include",
       signal: controller.signal,
       headers: {
-        Accept:
-          "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+        Accept: "application/json, text/plain, */*",
         "Accept-Encoding": "gzip, deflate, br",
         "Accept-Language": "en-US,en;q=0.9,pt-BR;q=0.8,pt;q=0.7,es;q=0.6",
         "Cache-Control": "max-age=0",
         Connection: "keep-alive",
-        //"x-flow-id": "95d90fee-5dcb-4d0f-b1a2-973daac6d5d9",
+        "x-latam-app-session-id": "4ef34bea-e030-47be-949c-f127ec3890d1",
+        "x-latam-application-country": "BR",
+        "x-latam-application-lang": "pt",
+        "x-latam-application-name": "web-air-offers",
+        "x-latam-application-oc": "br",
+        "x-latam-client-name": "web-air-offers",
+        "x-latam-request-id": "23ef105a-eb98-4428-8f61-c150634b9961",
+        "x-latam-track-id": "ca272c98-7763-4404-a550-5bf9dabcd8ad",
         "user-agent":
           "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.71 Safari/537.36",
-        cookie: cookie,
+        Cookie: cookie,
       },
     }
   ).catch((error) => {
@@ -50,13 +57,17 @@ for (const airportCode of airportCodes) {
     return null;
   });
 
-  cookie = flightsDataRes.headers.get("set-cookie");
+  cookies = flightsDataRes.headers.raw()["set-cookie"];
+  cookie = cookies.map((cookie) => cookie.split(";")[0]).join("; ");
 
   const flightsData = await flightsDataRes.json();
 
   if (!flightsData) continue;
-  const flight = flightsData.data[0].flights.filter(
-    (flight) => flight.flightCode === flightsData.data[0].recommendedFlightCode
+
+  console.log(flightsData);
+
+  const flight = flightsData.content.filter((flight) =>
+    flight.summary.tags.includes("RECOMMENDED")
   );
 
   flights.push(flight);
